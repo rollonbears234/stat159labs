7859e1d77642e7d1ebe634ae086cdb284bc7cc3b
Bryan Alcorn <irish.dancers.berkeley@gmail.com>
2016-10-10 12:35:40 -0700

    finished first round of testing

--- lab7/test-that/tests/test.R
+++ lab7/test-that/tests/test.R
@@ -1,16 +1,12 @@
 
 
 
-x <- c(1, 2, 3, 4, 5)
-y <- c(1, 2, 3, 4, NA)
-z <- c(TRUE, FALSE, TRUE)
-w <- letters[1:5]
 
-source("../functions/functions.R")
+source("../functions/range-value.R")
 
-context("Test for range value") 
+context("Test for range value")
 
-test_that("range works as expected" {
+test_that("range works as expected", {
   x <- c(1, 2, 3, 4, 5)
 
   expect_equal(range_value(x), 4)
@@ -18,4 +14,25 @@ test_that("range works as expected" {
   expect_type(range_value(x), 'double')
 })
 
+test_that("Tests for Y", {
+  y <- c(1, 2, 3, 4, NA)
+  expect_length(range_value(y), 1)
+  expect_equal(range_value(y), NA_real_)
 
+
+})
+
+
+test_that("Tests for Z", {
+  z <- c(TRUE, FALSE, TRUE)
+  expect_type(range_value(z), 'integer')
+  expect_length(range_value(z), 1)
+  expect_equal(range_value(z), 1L)
+
+})
+
+
+test_that("Tests for W", {
+  w <- letters[1:5]
+  expect_error(range_value(w), "non-numeric argument to binary operator")
+})
