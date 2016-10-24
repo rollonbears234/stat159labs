6d703e35691e6da1425150d5ae5d2a1d8be7f573
Bryan Alcorn <alcorn.bryan@gmail.com>
2016-10-24 12:45:06 -0700

    have basic template done

--- lab8/README.md
+++ lab8/README.md
@@ -1,1 +1,7 @@
-#### Lab 8 to make a simple app using the Shiny Library 
+#### Lab 8 to make a simple app using the Shiny Library
+- we will use advertising data to create three drop downs
+
+
+Your ShinyDoc will have a dropdown with a list of 3 options - TV, Radio and newspaper as an input element.
+Your output will be a scatter plot between sales and the input option chosen from the dropdown.
+You will write your code in a file called app.R
---
+++ lab8/app.R
@@ -0,0 +1,49 @@
+
+# 01-kmeans-app -> Histogram app
+library(shiny)
+
+palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
+  "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
+
+
+ui <- fluidPage(
+  headerPanel('Histogram of Advertising Data'),
+  sidebarPanel(
+    selectInput(inputId = "Media", label = "Choose a Media Outlet",
+    choices = list("Newspaper" = "Newspaper", "Radio" = "Radio", "TV" = "TV"),
+    ),
+  mainPanel(
+    plotOutput('scat')
+    )
+  )
+)
+
+server <- function(input, output) {
+
+  adData = read.csv(file = "../../data/Advertising.csv")
+  dataCurr = input$Media
+
+  output$scat <- renderPlot({
+    plot(adData$dataCurr, adData$sales)
+  })
+
+}
+
+shinyApp(ui = ui, server = server)
+
+
+#
+# ui <- fluidPage(
+#   sliderInput(inputId = "num",
+#     label = "Choose a number",
+#     value = 25, min = 1, max = 100),
+#   plotOutput("hist")
+# )
+#
+# server <- function(input, output) {
+#   output$hist <- renderPlot({
+#     hist(rnorm(input$num))
+#   })
+# }
+#
+# shinyApp(ui = ui, server = server)
